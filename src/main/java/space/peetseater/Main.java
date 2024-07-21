3d6cc783b817b44734829d52d9ae6dd7d72b4498
Ethan Eldridge <ejayeldridge@gmail.com>
2024-07-21 14:20:58 -0400

    Refactor: define extensions to swap in finals
    Because there's someone I know who might want to use this and would want
    to be able to set them as desired.

--- src/main/java/space/peetseater/Main.java
+++ src/main/java/space/peetseater/Main.java
@@ -11,10 +11,14 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.StandardCopyOption;
 
 public class Main {
+
+    static final String oldExtension = ".jfif";
+    static final String newExtension = ".jpg";
+
     public static void fixFile(Path path) throws IOException {
-        if (path.getFileName().toString().endsWith(".jfif")) {
-            String[] parts = path.toAbsolutePath().toString().split(".jfif");
-            Path newPath = Paths.get(parts[0]  + ".jpg");
+        if (path.getFileName().toString().endsWith(oldExtension)) {
+            String[] parts = path.toAbsolutePath().toString().split(oldExtension);
+            Path newPath = Paths.get(parts[0]  + newExtension);
             Files.move(path, newPath, StandardCopyOption.REPLACE_EXISTING);
         }
     }
@@ -24,7 +28,7 @@ public class Main {
         public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
             if (attrs.isRegularFile()) {
                 fixFile(file);
-            };
+            }
             return FileVisitResult.CONTINUE;
         }
     }
