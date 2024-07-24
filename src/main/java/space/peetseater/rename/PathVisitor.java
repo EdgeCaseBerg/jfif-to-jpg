44fc7466bed1480f723748bb58bd261e53054d52
Ethan Eldridge <ejayeldridge@gmail.com>
2024-07-24 00:28:35 -0400

    Add generic Path visitor
    Give it a consumer to run and it will go forth and hit every path you
    ask it to with the consumer action. If given a recursion boolean of true
    then it will go down the path, we could probably make it relatively
    simple to make the checks against whether or not it should accept a file
    and run the consumer or not, same with the way the recursiveness is
    checked.
    
    If we wanted to make it so that it doesn't other with hidden files or
    that sort of thing, then we'd probably want to make it behave that way.

---
+++ src/main/java/space/peetseater/rename/PathVisitor.java
@@ -0,0 +1,38 @@
+package space.peetseater.rename;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.function.Consumer;
+
+class PathVisitor extends SimpleFileVisitor<Path> {
+    private final Path startPath;
+    private final boolean isRecursive;
+    private final Consumer<Path> consumer;
+
+    public PathVisitor(Consumer<Path> pathConsumer, Path startPath, boolean recurse
+    ) {
+        this.consumer = pathConsumer;
+        this.startPath = startPath;
+        this.isRecursive = recurse;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        if (startPath.equals(dir)) return FileVisitResult.CONTINUE;
+        if (!isRecursive) {
+            return FileVisitResult.SKIP_SUBTREE;
+        }
+        return super.preVisitDirectory(dir, attrs);
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (attrs.isRegularFile()) {
+            this.consumer.accept(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
