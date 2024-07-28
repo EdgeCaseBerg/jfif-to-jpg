93d620de580332b1ff84ab0419321217e48281f6
Ethan Eldridge <ejayeldridge@gmail.com>
2024-07-28 14:08:50 -0400

    Make PathVisitor public
    so we can use it for CLI and GUI

--- src/main/java/space/peetseater/rename/PathVisitor.java
+++ src/main/java/space/peetseater/rename/PathVisitor.java
@@ -7,7 +7,7 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.function.Consumer;
 
-class PathVisitor extends SimpleFileVisitor<Path> {
+public class PathVisitor extends SimpleFileVisitor<Path> {
     private final Path startPath;
     private final boolean isRecursive;
     private final Consumer<Path> consumer;
