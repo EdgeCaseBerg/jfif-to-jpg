c4d8b4f4fe5559c5c3611e149b88597a2b0461a7
Ethan Eldridge <ejayeldridge@gmail.com>
2024-08-01 23:15:06 -0400

    Update ExtensionsList w/ rename of worker

--- src/main/java/space/peetseater/rename/gui/ExtensionsList.java
+++ src/main/java/space/peetseater/rename/gui/ExtensionsList.java
@@ -1,6 +1,9 @@
 package space.peetseater.rename.gui;
 
+import space.peetseater.rename.gui.workers.InputExtensionsListWorker;
+
 import javax.swing.*;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
@@ -15,7 +18,7 @@ public class ExtensionsList implements PropertyChangeListener {
     private final JLabel jLabel;
     private final String labelFormat = "%d Files Checked";
     private final JPanel jPanel;
-    private BackgroundFileExtensionWorker worker;
+    private InputExtensionsListWorker worker;
     int filesSeen = 0;
 
     public ExtensionsList(List<String> initialExtensions) {
@@ -42,11 +45,19 @@ public class ExtensionsList implements PropertyChangeListener {
         return this.jPanel;
     }
 
+    public void addListSelectionListener(ListSelectionListener listener) {
+        jList.addListSelectionListener(listener);
+    }
+
+    public String getSelectedListItem() {
+        return jList.getSelectedValue();
+    }
+
     public void calculateExtensions(Path path) {
         if (worker != null) {
             worker.cancel(true);
         }
-        worker = new BackgroundFileExtensionWorker(path);
+        worker = new InputExtensionsListWorker(path);
         worker.addPropertyChangeListener(this);
         worker.execute();
     }
@@ -57,8 +68,8 @@ public class ExtensionsList implements PropertyChangeListener {
             return;
         }
 
-        if (BackgroundFileExtensionWorker.PROGRESS_PROPERTY_NAME.equals(evt.getPropertyName())) {
-            updateLabelsFromProgressUpdate((BackgroundFileExtensionWorker.PartialResult) evt.getNewValue());
+        if (InputExtensionsListWorker.PROGRESS_PROPERTY_NAME.equals(evt.getPropertyName())) {
+            updateLabelsFromProgressUpdate((InputExtensionsListWorker.PartialResult) evt.getNewValue());
         }
 
         if ("state".equals(evt.getPropertyName())) {
@@ -85,7 +96,7 @@ public class ExtensionsList implements PropertyChangeListener {
         }
     }
 
-    private void updateLabelsFromProgressUpdate(BackgroundFileExtensionWorker.PartialResult partial) {
+    private void updateLabelsFromProgressUpdate(InputExtensionsListWorker.PartialResult partial) {
         filesSeen = partial.totalFilesChecked();
         jLabel.setText(labelFormat.formatted(partial.totalFilesChecked()));
         for (String ext : partial.extensionsSeen()) {
