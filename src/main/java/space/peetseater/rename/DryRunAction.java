7cdb4526cbb51a1febbe736fce0f6a506382009f
Ethan Eldridge <ejayeldridge@gmail.com>
2024-08-02 00:29:11 -0400

    Remove unnecessary list from DryRunAction

--- src/main/java/space/peetseater/rename/DryRunAction.java
+++ src/main/java/space/peetseater/rename/DryRunAction.java
@@ -1,7 +1,5 @@
 package space.peetseater.rename;
 
-import java.util.LinkedList;
-import java.util.function.Consumer;
 import java.util.logging.Logger;
 
 public class DryRunAction<T> implements ConditionalConsumer<T> {
@@ -9,19 +7,14 @@ public class DryRunAction<T> implements ConditionalConsumer<T> {
     public static Logger logger = Logger.getLogger(DryRunAction.class.toString());
 
     private final ConditionalConsumer<T> wrapped;
-    private final LinkedList<Audit<T>> auditTrail;
 
     public DryRunAction(ConditionalConsumer<T> wrapped) {
         this.wrapped = wrapped;
-        this.auditTrail = new LinkedList<Audit<T>>();
     }
-    
-    public void clearAuditTrail() {
-        this.auditTrail.clear();
-    }
-    
-    public LinkedList<Audit<T>> getAuditTrail() {
-        return this.auditTrail;
+
+    @Override
+    public String toString() {
+        return wrapped.toString() + " DRY RUN";
     }
 
     @Override
@@ -29,7 +22,6 @@ public class DryRunAction<T> implements ConditionalConsumer<T> {
         String wouldOrWouldNot = wrapped.shouldTakeAction(t) ? "" : "not ";
         String logMessage = "Would %stake action on %s with %s".formatted(wouldOrWouldNot, t, wrapped);
         logger.info(logMessage);
-        this.auditTrail.add(new Audit<>(t, logMessage));
     }
 
     @Override
