ef83385ef32396040464fe0d87702efb59278b71
Ethan Eldridge <ejayeldridge@gmail.com>
2024-08-02 00:27:53 -0400

    Add audit method to ConditionalConsumer

--- src/main/java/space/peetseater/rename/ConditionalConsumer.java
+++ src/main/java/space/peetseater/rename/ConditionalConsumer.java
@@ -4,4 +4,8 @@ import java.util.function.Consumer;
 
 public interface ConditionalConsumer<T> extends Consumer<T> {
     boolean shouldTakeAction(T t);
+    default Audit<T> getAudit(T t) {
+        String w = shouldTakeAction(t) ? "Would" : "Would not";
+        return new Audit<>(t, "%s %s on %s".formatted(w, toString(), t));
+    }
 }
