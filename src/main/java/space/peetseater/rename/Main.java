ed13d0de0443415702e0b94669bc2fbeb191729f
Ethan Eldridge <ejayeldridge@gmail.com>
2024-08-04 16:35:32 -0400

    Update Main method to call CLI and GUI
    Depending on if you pass --gui or not of course!

--- src/main/java/space/peetseater/rename/Main.java
+++ src/main/java/space/peetseater/rename/Main.java
@@ -1,52 +1,24 @@
 package space.peetseater.rename;
 
-import java.io.File;
+import space.peetseater.rename.gui.GraphicalUserInterfaceRunner;
+
+import javax.swing.*;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.FileVisitResult;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.StandardCopyOption;
+import java.util.logging.Logger;
 
 public class Main {
-
-    static final String oldExtension = ".jfif";
-    static final String newExtension = ".jpg";
-
-    public static void fixFile(Path path) throws IOException {
-        if (path.getFileName().toString().endsWith(oldExtension)) {
-            String[] parts = path.toAbsolutePath().toString().split(oldExtension);
-            Path newPath = Paths.get(parts[0]  + newExtension);
-            Files.move(path, newPath, StandardCopyOption.REPLACE_EXISTING);
-        }
-    }
-
-    static class FileRenameVisitor extends SimpleFileVisitor<Path> {
-        @Override
-        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-            if (attrs.isRegularFile()) {
-                fixFile(file);
-            }
-            return FileVisitResult.CONTINUE;
-        }
-    }
+    public static Logger logger = Logger.getLogger(Main.class.toString());
 
     public static void main(String[] args) throws IOException {
-        for (String filename : args) {
-            Path originalPath = Paths.get(filename);
-            File originalFile = originalPath.toFile();
-            if (!originalFile.exists()) {
-                System.out.printf("File does not exist: %s%n", filename);
-                continue;
-            }
+        boolean useGUI = Input.getGUIEnabled(args).orElse(false);
+        LoggingHelper.configureLogLevelsByInput(args, logger);
 
-            if (originalPath.toFile().isDirectory()) {
-                Files.walkFileTree(originalPath, new FileRenameVisitor());
-            } else {
-                fixFile(originalPath);
-            }
+        if(useGUI) {
+            logger.fine("Starting program in GUI mode, skipping CLI processing");
+            SwingUtilities.invokeLater(new GraphicalUserInterfaceRunner(args));
+        } else {
+            logger.fine("Starting program in CLI mode");
+            CommandLineInterface.run(args);
         }
     }
 }
